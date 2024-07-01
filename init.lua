2cffa66701b31e60ebb9108fe7e25f16702fb45a
mutluerengazi <mutluerengazi@gmail.com>
2024-07-01 17:07:55 +0300

    feat updt

--- init.lua
+++ init.lua
@@ -830,9 +830,6 @@ require('lazy').setup({
       -- set use_icons to true if you have a Nerd Font
       statusline.setup { use_icons = vim.g.have_nerd_font }
 
-      --tabs for opened buffers
-      require('mini.tabline').setup()
-
       -- You can configure sections in the statusline by overriding their
       -- default behavior. For example, here we set the section for
       -- cursor location to LINE:COLUMN
