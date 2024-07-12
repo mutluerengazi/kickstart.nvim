e01f1f325815cbd56a1f25d5290c7b3deb090498
mutluerengazi <mutluerengazi@gmail.com>
2024-07-12 09:17:25 +0300

    feat: starter deleted

--- lua/kickstart/plugins/mini.lua
+++ lua/kickstart/plugins/mini.lua
@@ -7,30 +7,5 @@ return {
     require('mini.surround').setup()
     -- Git setup
     require('mini.git').setup()
-    -- Starter setup
-    require('mini.starter').setup {
-      evaluate_single = true,
-      header = table.concat({
-        [[                                                ]],
-        [[ ███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗ ]],
-        [[ ████╗  ██║ ██╔════╝██╔═══██╗ ██║   ██║ ██║ ████╗ ████║ ]],
-        [[ ██╔██╗ ██║ █████╗  ██║   ██║ ██║   ██║ ██║ ██╔████╔██║ ]],
-        [[ ██║╚██╗██║ ██╔══╝  ██║   ██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║ ]],
-        [[ ██║ ╚████║ ███████╗╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║ ]],
-        [[ ╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝ ]],
-        [[                                                ]],
-      }, '\n'),
-      footer = table.concat({
-        '',
-        '[ TIP: To exit Vim, just power off your computer. ]',
-      }, '\n'),
-      items = {
-        { name = 'Find File', action = 'Telescope find_files', section = 'Telescope' },
-        { name = 'Find Word', action = 'Telescope live_grep', section = 'Telescope' },
-        { name = 'Recent Files', action = 'Telescope oldfiles', section = 'Telescope' },
-        { name = 'Config', action = 'lua require("lazyvim.util").telescope.config_files()()', section = 'Config' },
-        { name = 'Quit', action = 'quit', section = 'Actions' },
-      },
-    }
   end,
 }
