ec2ca5e328a600e5d5575bd9272bc26f6d914b4b
mutluerengazi <mutluerengazi@gmail.com>
2024-07-28 18:34:26 +0300

    update

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -25,7 +25,7 @@ return {
         hide_gitignored = false, -- Optional: This line shows gitignored files
       },
       window = {
-        position = 'right',
+        position = 'float',
         mappings = {
           ['\\'] = 'close_window',
           ['C'] = 'close_all_nodes',
