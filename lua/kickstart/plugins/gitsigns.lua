3e405b87d9336fe38b43c86a83b73ac245d053f8
mutluerengazi <mutluerengazi@gmail.com>
2024-07-29 23:51:52 +0300

    update

--- lua/kickstart/plugins/gitsigns.lua
+++ lua/kickstart/plugins/gitsigns.lua
@@ -10,49 +10,66 @@ return {
         changedelete = { text = '~' },
       },
       on_attach = function(bufnr)
-        local gs = package.loaded.gitsigns
+        local gitsigns = require 'gitsigns'
+
         local function map(mode, l, r, opts)
           opts = opts or {}
           opts.buffer = bufnr
           vim.keymap.set(mode, l, r, opts)
         end
+
         -- Navigation
         map('n', ']c', function()
-          if vim.wo.diff then return ']c' end
-          vim.schedule(function() gs.next_hunk() end)
-          return '<Ignore>'
-        end, { expr = true, desc = 'Next hunk' })
+          if vim.wo.diff then
+            vim.cmd.normal { ']c', bang = true }
+          else
+            gitsigns.next_hunk()
+          end
+        end, { desc = 'Next hunk' })
         map('n', '[c', function()
-          if vim.wo.diff then return '[c' end
-          vim.schedule(function() gs.prev_hunk() end)
-          return '<Ignore>'
-        end, { expr = true, desc = 'Previous hunk' })
+          if vim.wo.diff then
+            vim.cmd.normal { '[c', bang = true }
+          else
+            gitsigns.prev_hunk()
+          end
+        end, { desc = 'Previous hunk' })
+
         -- Actions
-        map('n', '<leader>hs', gs.stage_hunk, { desc = 'Stage hunk' })
-        map('n', '<leader>hr', gs.reset_hunk, { desc = 'Reset hunk' })
-        map('v', '<leader>hs', function() gs.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end, { desc = 'Stage hunk' })
-        map('v', '<leader>hr', function() gs.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end, { desc = 'Reset hunk' })
-        map('n', '<leader>hS', gs.stage_buffer, { desc = 'Stage buffer' })
-        map('n', '<leader>hu', gs.undo_stage_hunk, { desc = 'Undo stage hunk' })
-        map('n', '<leader>hR', gs.reset_buffer, { desc = 'Reset buffer' })
-        map('n', '<leader>hp', gs.preview_hunk, { desc = 'Preview hunk' })
-        map('n', '<leader>hb', function() gs.blame_line{full=true} end, { desc = 'Blame line' })
-        map('n', '<leader>tb', gs.toggle_current_line_blame, { desc = 'Toggle line blame' })
-        map('n', '<leader>hd', gs.diffthis, { desc = 'Diff this' })
-        map('n', '<leader>hD', function() gs.diffthis('~') end, { desc = 'Diff this ~' })
-        map('n', '<leader>td', gs.toggle_deleted, { desc = 'Toggle deleted' })
+        map('n', '<leader>hs', gitsigns.stage_hunk, { desc = 'Stage hunk' })
+        map('n', '<leader>hr', gitsigns.reset_hunk, { desc = 'Reset hunk' })
+        map('v', '<leader>hs', function()
+          gitsigns.stage_hunk { vim.fn.line '.', vim.fn.line 'v' }
+        end, { desc = 'Stage hunk' })
+        map('v', '<leader>hr', function()
+          gitsigns.reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
+        end, { desc = 'Reset hunk' })
+        map('n', '<leader>hS', gitsigns.stage_buffer, { desc = 'Stage buffer' })
+        map('n', '<leader>hu', gitsigns.undo_stage_hunk, { desc = 'Undo stage hunk' })
+        map('n', '<leader>hR', gitsigns.reset_buffer, { desc = 'Reset buffer' })
+        map('n', '<leader>hp', gitsigns.preview_hunk, { desc = 'Preview hunk' })
+        map('n', '<leader>hb', function()
+          gitsigns.blame_line { full = true }
+        end, { desc = 'Blame line' })
+        map('n', '<leader>tb', gitsigns.toggle_current_line_blame, { desc = 'Toggle line blame' })
+        map('n', '<leader>hd', gitsigns.diffthis, { desc = 'Diff this' })
+        map('n', '<leader>hD', function()
+          gitsigns.diffthis '~'
+        end, { desc = 'Diff this ~' })
+        map('n', '<leader>td', gitsigns.toggle_deleted, { desc = 'Toggle deleted' })
+
         -- Text object
-        map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>', { desc = 'Select hunk' })
+        map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>', { desc = 'Select hunk' })
 
-        -- New function for full file blame
+        -- Full file blame (custom addition)
         local function full_blame()
-          vim.cmd('G blame')
+          local file = vim.fn.expand '%:p'
+          vim.cmd('vnew | read !git blame ' .. vim.fn.shellescape(file))
+          vim.cmd 'setlocal buftype=nofile bufhidden=wipe nobuflisted nomodifiable'
+          vim.cmd 'setlocal nonumber'
+          vim.cmd 'setlocal filetype=git'
         end
-
-        -- New keybinding for full file blame
-        map('n', '<leader>hB', full_blame, { desc = "Full file blame" })
+        map('n', '<leader>hB', full_blame, { desc = 'Full file blame' })
       end,
     },
   },
 }
-
