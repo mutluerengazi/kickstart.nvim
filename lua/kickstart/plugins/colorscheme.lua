6fc2c5a36f663053e005916ca325275856a627dd
mutluerengazi <mutluerengazi@gmail.com>
2024-08-01 19:23:46 +0300

    updt

--- lua/kickstart/plugins/colorscheme.lua
+++ lua/kickstart/plugins/colorscheme.lua
@@ -1,11 +1,13 @@
 return {
   {
-    'sainnhe/everforest',
+    'sainnhe/gruvbox-material',
     lazy = false,
     priority = 1000,
     config = function()
-      vim.g.everforest_enable_italic = false
-      vim.cmd.colorscheme 'everforest'
+      -- Optionally configure and load the colorscheme
+      -- directly inside the plugin declaration.
+      vim.g.gruvbox_material_enable_italic = true
+      vim.cmd.colorscheme 'gruvbox-material'
     end,
   },
 }
