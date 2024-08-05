959e8ebd690599d8deed6e9081cd30dc65c5b2de
mutluerengazi <mutluerengazi@gmail.com>
2024-08-05 12:11:10 +0300

    asd

--- lua/kickstart/plugins/colorscheme.lua
+++ lua/kickstart/plugins/colorscheme.lua
@@ -1,37 +1,11 @@
 return {
   {
-    'catppuccin/nvim',
-    name = 'catppuccin',
+    'sainnhe/gruvbox-material',
+    lazy = false,
     priority = 1000,
     config = function()
-      require('catppuccin').setup {
-        flavour = 'mocha', -- Can be: latte, frappe, macchiato, mocha
-        background = { light = 'latte', dark = 'mocha' },
-        transparent_background = false,
-        show_end_of_buffer = false,
-        term_colors = true,
-        dim_inactive = {
-          enabled = false,
-          shade = 'dark',
-          percentage = 0.15,
-        },
-        no_italic = true,
-        no_bold = false,
-        color_overrides = {},
-        custom_highlights = {},
-        integrations = {
-          cmp = true,
-          gitsigns = true,
-          nvimtree = false,
-          neotree = true,
-          telescope = true,
-          notify = false,
-          mini = false,
-        },
-      }
-
-      -- Set the colorscheme
-      vim.cmd.colorscheme 'catppuccin'
+      vim.g.gruvbox_material_enable_italic = false
+      vim.cmd.colorscheme 'gruvbox-material'
     end,
   },
 }
