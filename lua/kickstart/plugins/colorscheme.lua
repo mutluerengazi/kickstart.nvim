52fa5bc21aa1e57480eee3c11df73922d9105d28
mutluerengazi <mutluerengazi@gmail.com>
2024-08-19 11:29:46 +0300

    feat: update

--- lua/kickstart/plugins/colorscheme.lua
+++ lua/kickstart/plugins/colorscheme.lua
@@ -1,15 +1,14 @@
 return {
   {
-    'folke/tokyonight.nvim',
+    'Shatur/neovim-ayu',
     lazy = false,
     priority = 1000,
-    opts = {},
-    init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-storm'
-
+    config = function()
+      require('ayu').setup({
+        mirage = false, -- Set to true if you want to use the mirage variant
+        overrides = {}, -- A dictionary of group names, each associated with a dictionary of parameters to override
+      })
+      vim.cmd.colorscheme 'ayu-dark'
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
     end,
