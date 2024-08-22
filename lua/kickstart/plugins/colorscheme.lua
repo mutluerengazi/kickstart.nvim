bfe0cf9e7a545599f88fff2ec11533534d56de54
mutluerengazi <mutluerengazi@gmail.com>
2024-08-22 15:53:50 +0300

    df

--- lua/kickstart/plugins/colorscheme.lua
+++ lua/kickstart/plugins/colorscheme.lua
@@ -1,14 +1,14 @@
 return {
   {
-    'Shatur/neovim-ayu',
+    "ellisonleao/gruvbox.nvim",
     lazy = false,
     priority = 1000,
     config = function()
-      require('ayu').setup({
+      require('gruvbox').setup({
         mirage = false, -- Set to true if you want to use the mirage variant
         overrides = {}, -- A dictionary of group names, each associated with a dictionary of parameters to override
       })
-      vim.cmd.colorscheme 'ayu-dark'
+      vim.cmd.colorscheme 'gruvbox'
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
     end,
