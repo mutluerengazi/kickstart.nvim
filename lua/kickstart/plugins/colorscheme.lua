3b2f314929dddadab5828f5cb4ed432e09f12eb1
mutluerengazi <mutluerengazi@gmail.com>
2024-09-10 14:22:58 +0300

    update

--- lua/kickstart/plugins/colorscheme.lua
+++ lua/kickstart/plugins/colorscheme.lua
@@ -1,14 +1,14 @@
 return {
   {
-    'shaunsingh/nord.nvim',
+    'Shatur/neovim-ayu',
     lazy = false,
     priority = 1000,
     config = function()
-      -- Load the colorscheme
-      vim.cmd [[colorscheme nord]]
-
-      -- Optional: Additional configuration
-      require('nord').set()
+      require('ayu').setup({
+        mirage = false, -- Set to true if you want to use the mirage variant
+        overrides = {}, -- A dictionary of group names, each associated with a dictionary of parameters to override
+      })
+      vim.cmd.colorscheme 'ayu-dark'
     end,
   },
 }
