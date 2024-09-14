630ece48b98177bf5b851c9dddf000291ef95ff0
mutluerengazi <mutluerengazi@gmail.com>
2024-09-14 11:00:55 +0300

    feat: update

--- lua/kickstart/plugins/colorscheme.lua
+++ lua/kickstart/plugins/colorscheme.lua
@@ -1,14 +1,10 @@
 return {
   {
-    'Shatur/neovim-ayu',
+    'ellisonleao/gruvbox.nvim',
     lazy = false,
     priority = 1000,
     config = function()
-      require('ayu').setup({
-        mirage = false, -- Set to true if you want to use the mirage variant
-        overrides = {}, -- A dictionary of group names, each associated with a dictionary of parameters to override
-      })
-      vim.cmd.colorscheme 'ayu-dark'
+      vim.cmd.colorscheme 'gruvbox'
     end,
   },
 }
