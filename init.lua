527a660e3b1b715d15b849ea7d24cafe3b485dca
mutluerengazi <mutluerengazi@gmail.com>
2024-09-23 12:05:40 +0300

    update

--- init.lua
+++ init.lua
@@ -509,7 +509,7 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
-        ["typescript-language-server"] = {},
+        ts_ls = {},
         --
 
         lua_ls = {
