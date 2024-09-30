f64b62624908c82b457cffca469f0a80db191e6c
mutluerengazi <mutluerengazi@gmail.com>
2024-09-30 18:02:55 +0300

    color up

--- lua/kickstart/plugins/colorscheme.lua
+++ lua/kickstart/plugins/colorscheme.lua
@@ -1,11 +1,11 @@
 return {
-  'sainnhe/everforest',
+  'navarasu/onedark.nvim',
   lazy = false,
   priority = 1000,
   config = function()
     -- Optionally configure and load the colorscheme
     -- directly inside the plugin declaration.
     vim.g.everforest_enable_italic = true
-    vim.cmd.colorscheme('everforest')
+    vim.cmd.colorscheme('onedark')
   end
 }
