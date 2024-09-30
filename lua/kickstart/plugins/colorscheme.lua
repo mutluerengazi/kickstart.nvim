b7ac568e1a6d6394a6a04994727451a6f69aa50f
mutluerengazi <mutluerengazi@gmail.com>
2024-09-30 14:49:27 +0300

    update

--- lua/kickstart/plugins/colorscheme.lua
+++ lua/kickstart/plugins/colorscheme.lua
@@ -1,11 +1,11 @@
 return {
-  "catppuccin/nvim",
+  'sainnhe/everforest',
   lazy = false,
   priority = 1000,
   config = function()
     -- Optionally configure and load the colorscheme
     -- directly inside the plugin declaration.
-    -- vim.g.gruvbox_material_enable_italic = true
-    vim.cmd.colorscheme('catppuccin-mocha')
+    vim.g.everforest_enable_italic = true
+    vim.cmd.colorscheme('everforest')
   end
 }
