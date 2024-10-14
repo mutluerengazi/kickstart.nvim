a6583dd83979fb599d7698a4c5417efddb8c7520
mutluerengazi <mutluerengazi@gmail.com>
2024-10-14 21:34:15 +0300

    feat: color update

--- lua/kickstart/plugins/colorscheme.lua
+++ lua/kickstart/plugins/colorscheme.lua
@@ -1,11 +1,11 @@
 return {
-  "catppuccin/nvim",
+  "ellisonleao/gruvbox.nvim",
   lazy = false,
   priority = 1000,
   config = function()
     -- Optionally configure and load the colorscheme
     -- directly inside the plugin declaration.
     -- vim.g.gruvbox_material_enable_italic = true
-    vim.cmd.colorscheme('catppuccin-mocha')
+    vim.cmd.colorscheme('gruvbox')
   end
 }
