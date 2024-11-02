6e9619d0c23400c8d868e2d486ee24e5f190ff00
mutluerengazi <mutluerengazi@gmail.com>
2024-11-02 16:18:12 +0300

    feat: update

--- lua/kickstart/plugins/copilotchat.lua
+++ lua/kickstart/plugins/copilotchat.lua
@@ -9,6 +9,7 @@ return {
     build = "make tiktoken",       -- Only on MacOS or Linux
     opts = {
       debug = true,                -- Enable debugging
+      model = "claude-3.5-sonnet"
       -- See Configuration section for rest
     },
     -- See Commands section for default commands if you want to lazy load on them
