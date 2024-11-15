d32549c84d993edaa28b383c4fba38dac7ce827b
erengazi <mutluerengazi@gmail.com>
2024-11-15 11:05:58 +0300

    feat: update

--- lua/kickstart/plugins/colorscheme.lua
+++ lua/kickstart/plugins/colorscheme.lua
@@ -1,9 +1,8 @@
 return {
-  "ellisonleao/gruvbox.nvim",
+  "dracula/vim",
   priority = 1000, -- Ensure it loads first
   lazy = false,
   config = function()
-    vim.cmd.colorscheme('gruvbox')
-    vim.o.background = 'dark'
+    vim.cmd.colorscheme('dracula')
   end
 }
