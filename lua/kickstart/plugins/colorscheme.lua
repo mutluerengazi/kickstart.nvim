6a7ae65434569a11cd6af1a97057671b092c8aed
erengazi <mutluerengazi@gmail.com>
2024-11-26 15:03:33 +0300

    feat: color update

--- lua/kickstart/plugins/colorscheme.lua
+++ lua/kickstart/plugins/colorscheme.lua
@@ -1,8 +1,11 @@
 return {
-  "folke/tokyonight.nvim",
-  priority = 1000, -- Ensure it loads first
+  'sainnhe/gruvbox-material',
   lazy = false,
+  priority = 1000,
   config = function()
-    vim.cmd.colorscheme('tokyonight-night')
+    -- Optionally configure and load the colorscheme
+    -- directly inside the plugin declaration.
+    -- vim.g.gruvbox_material_enable_italic = true
+    vim.cmd.colorscheme('gruvbox-material')
   end
 }
