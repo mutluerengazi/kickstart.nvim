59c888aa7e8a3f9e11e622dc5e5c0cc95a4fc5ee
erengazi <mutluerengazi@gmail.com>
2024-11-29 11:00:53 +0300

    updt

--- lua/kickstart/plugins/colorscheme.lua
+++ lua/kickstart/plugins/colorscheme.lua
@@ -1,8 +1,8 @@
 return {
-  "catppuccin/nvim",
+  "folke/tokyonight.nvim",
   lazy = false,
   priority = 1000,
   config = function()
-    vim.cmd.colorscheme('catppuccin-mocha')
+    vim.cmd.colorscheme('tokyonight-night')
   end
 }
--- lua/kickstart/plugins/lualine.lua
+++ lua/kickstart/plugins/lualine.lua
@@ -2,7 +2,6 @@ return {
   'nvim-lualine/lualine.nvim',
   dependencies = {
     'nvim-tree/nvim-web-devicons',
-    'catppuccin/nvim',
   },
   config = function()
     require('lualine').setup {
