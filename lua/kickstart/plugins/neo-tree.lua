f420d57c3c2c94c8c5b80f237aec054c4c7f383d
erengazi <mutluerengazi@gmail.com>
2024-11-29 15:46:42 +0300

    hopefully last update of colorscheme
    bet

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -12,7 +12,7 @@ return {
     {
       '<leader>n',
       function()
-        require('neo-tree.command').execute { toggle = true, dir = vim.loop.cwd() }
+        require('neo-tree.command').execute { toggle = false, dir = vim.fn.getcwd() }
       end,
       { desc = 'Toggle NeoTree' },
     },
@@ -20,12 +20,12 @@ return {
   opts = {
     filesystem = {
       filtered_items = {
-        visible = true, -- This line makes hidden files visible
-        hide_dotfiles = false, -- This line ensures dotfiles are not hidden
+        visible = true,          -- This line makes hidden files visible
+        hide_dotfiles = false,   -- This line ensures dotfiles are not hidden
         hide_gitignored = false, -- Optional: This line shows gitignored files
       },
       window = {
-        position = 'float',
+        position = 'left',
         mappings = {
           ['\\'] = 'close_window',
           ['C'] = 'close_all_nodes',
