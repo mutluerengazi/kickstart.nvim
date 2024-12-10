e68fdfd16b74793556e0a46091c51c62f829051f
erengazi <mutluerengazi@gmail.com>
2024-12-10 17:17:22 +0300

    updt

--- lua/kickstart/plugins/colorscheme.lua
+++ lua/kickstart/plugins/colorscheme.lua
@@ -1,9 +1,8 @@
 return {
-  'projekt0n/github-nvim-theme',
-  name = 'github-theme',
+  "ellisonleao/gruvbox.nvim",
   lazy = false,
   priority = 1000,
   config = function()
-    vim.cmd.colorscheme('github_dark_default')
+    vim.cmd.colorscheme('gruvbox')
   end
 }
