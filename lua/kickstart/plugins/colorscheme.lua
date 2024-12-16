cb3a1e35cc4836bf6c1acdddd603f19971763395
erengazi <mutluerengazi@gmail.com>
2024-12-16 11:59:00 +0300

    feat: update

--- lua/kickstart/plugins/colorscheme.lua
+++ lua/kickstart/plugins/colorscheme.lua
@@ -1,8 +1,8 @@
 return {
-  'shaunsingh/nord.nvim',
+  'projekt0n/github-nvim-theme',
   lazy = false,
   priority = 1000,
   config = function()
-    vim.cmd.colorscheme('nord')
+    vim.cmd.colorscheme('github_light')
   end
 }
