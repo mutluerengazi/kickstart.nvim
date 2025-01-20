8c1a3bc6bbb426a36d02634138d9491abe54b366
erengazi <mutluerengazi@gmail.com>
2025-01-20 10:22:07 +0300

    feat: update

--- lua/kickstart/plugins/colorscheme.lua
+++ lua/kickstart/plugins/colorscheme.lua
@@ -1,8 +1,8 @@
 return {
-  "gbprod/nord.nvim",
-  name = "nord",
+  "catppuccin/nvim",
+  name = "catppuccin",
   priority = 1000,
   config = function()
-    vim.cmd.colorscheme("nord")
+    vim.cmd.colorscheme("catppuccin-mocha")
   end
 }
